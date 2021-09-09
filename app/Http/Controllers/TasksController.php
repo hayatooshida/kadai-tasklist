e8fb664adf465b031ce3d52751e340c06095338c
hayato.oshida <umeboshi8810@yahoo.co.jp>
2021-09-09 10:01:41 +0000

    status add

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -46,10 +46,12 @@ class TasksController extends Controller
     {
         $request->validate([
             'content' => 'required|max:255',
+            'status' => 'required|max:10',
         ]);
         
         $task = new Task;
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
         
         return redirect('/');
@@ -95,11 +97,13 @@ class TasksController extends Controller
     {   
         $request->validate([
             'content' => 'required|max:255',
+            'status' => 'required|max:10',
         ]);
         
         $task = Task::findOrFail($id);
         
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
         
         return redirect('/');
